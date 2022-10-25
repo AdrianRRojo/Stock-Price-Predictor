6ae93d3b2bf058ef62b2927cd8bc378d28ea047c
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-25 18:46:03 -0500

    Exported a graph of paypals sock trend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+
+#%%
 import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
 # CSV was downloaded from https://finance.yahoo.com/quote/PYPL?p=PYPL&.tsrc=fin-srch -> provides data such as what the stock opened at, What the high and low price was and what the stock closed at. I will be using the date and what the stock closed at for that date.
-# Note: Vs code extension 'SandDance' is very useful, and is what I used during the course of this project.
+# Note: The Vs code extension 'Jupyter' is very useful, and is what I used during the course of this project.
 df = pd.read_csv('PYPL.csv') 
 
 
@@ -10,8 +14,6 @@ df
 df = df[['Date', 'Close']]
 df
 
-import datetime
-
 def str_to_datetime(s):
   split = s.split('-')
   year,month,day = int(split[0]),int(split[1]),int(split[2])
@@ -19,3 +21,10 @@ def str_to_datetime(s):
   return datetime.datetime(year=year,month=month, day=day)
 datetime_object = str_to_datetime('2015-07-06')
 datetime_object
+
+df.index = df.pop('Date')
+df
+
+plt.plot(df.index, df['Close'])
+
+# %%
