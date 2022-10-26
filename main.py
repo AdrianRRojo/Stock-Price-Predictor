ca066ef015cd96a9b00e3912ca913bb29966ae95
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-26 16:14:35 -0500

    added comments, removed import tensorflow.. will revisit issue tommorrow"

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 #%%
 import numpy as np
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
+# import tensorflow as tf
 # CSV was downloaded from https://finance.yahoo.com/quote/PYPL?p=PYPL&.tsrc=fin-srch -> provides data such as what the stock opened at, What the high and low price was and what the stock closed at. I will be using the date and what the stock closed at for that date.
 
 # Note: The Vs code extension 'Jupyter' is very useful, and is what I used during the course of this project.
@@ -11,12 +11,15 @@ import matplotlib.pyplot as plt
 #TODO: currently the graph is set up to recieve data from downloaded .csv files. in the future I would like to redesign this to recieve data via links and export graphs
 
 df = pd.read_csv('PYPL.csv') 
+# df
 
+#filters the data from the csv and shows only The closing price and the date
 df = df[['Date', 'Close']]
-df
+# df
 
-def str_to_datetime(s):
-  split = s.split('-')
+# converts a string to a date i.e "2015-07-06"
+def str_to_datetime(dateString):
+  split = dateString.split('-')
   year,month,day = int(split[0]),int(split[1]),int(split[2])
 
   return datetime.datetime(year=year,month=month, day=day)
@@ -24,7 +27,7 @@ datetime_object = str_to_datetime('2015-07-06')
 datetime_object
 
 df['Date'] = df['Date'].apply(str_to_datetime)
-df['Date']
+# df['Date']
 
 # Replaces the index of the chart with the date instead
 df.index = df.pop('Date')
@@ -36,6 +39,7 @@ plt.plot(df.index, df['Close'])
 
 # LSTM
 
+# select 2 dates from the stock that will be used for testing data 
 def time_between(dataframe, first_day, last_day, n=3):
   first_day = str_to_datetime(first_day)
   last_day  = str_to_datetime(last_day)
@@ -91,6 +95,7 @@ def time_between(dataframe, first_day, last_day, n=3):
 
   return new_df
 
+# We will now provide dateframe, time frame, and how many target results (3)
 df_time_between = time_between(df, '2015-10-08', '2022-10-24', n=3)
 df_time_between
 
